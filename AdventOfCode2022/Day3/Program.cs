25120d784f5bb6fc34075bbbcba9e6ce0868ebe5
Chris Green <chris.green@teradyne.com>
2022-12-04 08:35:58 -0600

    day4

--- AdventOfCode2022/Day3/Program.cs
+++ AdventOfCode2022/Day3/Program.cs
@@ -5,6 +5,7 @@ using System.IO;
 namespace AOC2022 {
     public class Day3 : DayN {
 
+        Rucksack rucksack;
 
         static void Main( string[] args ) {
             Day3 prog = new Day3("../../../example.txt");
@@ -13,15 +14,74 @@ namespace AOC2022 {
         }
 
         public Day3( string file ) {
+            rucksack = new Rucksack(file);
         }
 
         public override string Part1() {
-            return "";
+            return rucksack.parseContentPriorities().ToString();
         }
 
         public override string Part2() {
-            return "";
+            return rucksack.parseGroupBadges().ToString();
         }
+    }
+
+    public class Rucksack {
+
+        string[] lines;
+
+        public Rucksack(string file) {
+            lines = File.ReadAllLines(file);
+    
+        }
+
+        public int parseContentPriorities() {
+
+            int priority = 0;
+
+            foreach (string line in lines) {
+                string l = line.Substring(0, line.Length / 2);
+                string r = line.Substring(line.Length / 2);
+
+                IEnumerable<char> both = l.Intersect(r);
+
+                if ( both.Count() == 1 ) {
+                    char val = both.First();
+                    if ( val >= 'a' ) {
+                        priority += val - 'a' + 1;
+                    }
+                    else {
+                        priority += val - 'A' + 27;
+                    }
+                }
+            }
+
+            return priority;
+        }
+
+        public int parseGroupBadges() {
+
+            int badgeVal = 0;
+
+            for ( int i = 0; i < lines.Length; i += 3 ) {
+                IEnumerable<char> oneAndTwo = lines[i].Intersect(lines[i + 1]);
+                IEnumerable<char> all = oneAndTwo.Intersect(lines[i + 2]);
+
+                if (all.Count() == 1) {
+                    char val = all.First();
+                    if (val >= 'a') {
+                        badgeVal += val - 'a' + 1;
+                    }
+                    else {
+                        badgeVal += val - 'A' + 27;
+                    }
+                }
+
+            }
+
+            return badgeVal;
+        }
+
     }
 
 }
---
+++ AdventOfCode2022/Day4/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AOC2022 {
+    public class Day4 : DayN {
+
+        Assignments assignments;
+
+        static void Main( string[] args ) {
+            Day4 prog = new Day4("../../../example.txt");
+            prog.Debug = false;
+            prog.Run();
+        }
+
+        public Day4( string file ) {
+            assignments = new Assignments(file);
+        }
+
+        public override string Part1() {
+            return assignments.getAllFullyContained().ToString();
+        }
+
+        public override string Part2() {
+            return assignments.getAllOverlapping().ToString();
+        }
+    }
+
+    public class Assignments {
+
+        string[] lines;
+        
+        public Assignments(string file) {
+            lines = File.ReadAllLines(file);
+        }
+
+        public int getAllFullyContained() {
+            int cnt = 0;
+
+            foreach (string line in lines) {
+                string[] parts = line.Split(new char[] { '-', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (int.Parse(parts[0]) >= int.Parse(parts[2]) && int.Parse(parts[1]) <= int.Parse(parts[3])) {
+                    cnt++;
+                }
+                else if (int.Parse(parts[2]) >= int.Parse(parts[0]) && int.Parse(parts[3]) <= int.Parse(parts[1])) {
+                    cnt++;
+                }
+
+            }
+
+            return cnt;
+        }
+
+        public int getAllOverlapping() {
+            int cnt = 0;
+
+            foreach (string line in lines) {
+                string[] parts = line.Split(new char[] { '-', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if ( int.Parse(parts[0]) >= int.Parse(parts[2]) && int.Parse(parts[0]) <= int.Parse(parts[3])) {
+                    cnt++;
+                }
+                else if ( int.Parse(parts[1]) >= int.Parse(parts[2]) && int.Parse(parts[1]) <= int.Parse(parts[3]) ) {
+                    cnt++;
+                }
+                else if (int.Parse(parts[2]) >= int.Parse(parts[0]) && int.Parse(parts[2]) <= int.Parse(parts[1])) {
+                    cnt++;
+                }
+                else if (int.Parse(parts[3]) >= int.Parse(parts[0]) && int.Parse(parts[3]) <= int.Parse(parts[1])) {
+                    cnt++;
+                }
+
+            }
+
+            return cnt;
+        }
+
+    }
+}
